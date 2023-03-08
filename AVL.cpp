942b5c812d48be9a996e1630682da71264bf2d9a
Thea McCallie <tmccallie@ucdavis.edu>
2023-03-08 14:13:30 -0800

    added new function declarations and included the blank version defenttions

--- AVL.cpp
+++ AVL.cpp
@@ -19,3 +19,48 @@ void AVL::leftRotation ()
 {
 
 };
+
+int AVL::findTreeHeight ()
+{
+
+};
+
+int AVL::findRootKey ()
+{ 
+
+};
+
+int AVL::findTreeSize()
+{ 
+
+};
+
+int AVL::findNodeHeight () 
+{ 
+
+};
+
+int AVL::findNodeBalanceFactor () 
+{ 
+
+};
+
+int AVL::findNodeParent ()
+{
+
+};
+
+int AVL::findNodeLeftChild () 
+{
+
+};
+
+int AVL::findNodeRightChild ()
+{
+
+};
+
+bool AVL::isNodeRoot ()
+{
+    
+};
--- AVL.h
+++ AVL.h
@@ -7,4 +7,14 @@ class AVL : public BST
     void updateHeight ();
     void rightRotation ();
     void leftRotation ();
+
+    int findTreeHeight(); 
+    int findRootKey(); 
+    int findTreeSize();
+    int findNodeHeight();
+    int findNodeBalanceFactor(); 
+    int findNodeParent(); 
+    int findNodeLeftChild(); 
+    int findNodeRightChild(); 
+    bool isNodeRoot(); 
 };
